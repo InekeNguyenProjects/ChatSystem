911d1f3732f8bf6d2df682bb9d149878b2fc81a6
mikenkie <ineke.kim@gmail.com>
2024-04-29 14:11:36 -0400

    Server chat to form connection and create the hnadler thread to handle the clients for the server

--- src/ServerChat.java
+++ src/ServerChat.java
@@ -8,10 +8,33 @@ import java.util.Vector;
 
 public class ServerChat {
 	
-    static Vector<ServerHandler> sockets = new Vector <ServerHandler>();
+    static Vector <ServerHandler> sockets = new Vector <ServerHandler>();
     
 
-
+    public static void main (String args[]) throws IOException
+    {
+        ServerSocket socket = new ServerSocket(2020); //Server socket running at port 2020
+        int i = 0;
+        
+        System.out.println("Server is waiting for connections");
+        
+        while (true) {
+            System.out.println("Current Client Count - "+ sockets.size());
+            
+            Socket connection = socket.accept();
+            String name = "Client - " + i;
+            
+            PrintWriter output = new PrintWriter(connection.getOutputStream(),true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            ServerHandler handler = new ServerHandler(name, connection, output, input);
+            
+            sockets.add(handler);
+            Thread myThread = new Thread(handler);
+            myThread.start();
+            
+            ++i;
+        }
+    }
 
 
 }
