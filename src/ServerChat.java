f6935c254273f9890f83a8d70a7d6d2972c9d3d3
mikenkie <ineke.kim@gmail.com>
2024-04-29 14:01:45 -0400

    moved to ServerHandler class

--- src/ServerChat.java
+++ src/ServerChat.java
@@ -6,67 +6,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Vector;
 
-public class ServerChat implements Runnable {
+public class ServerChat {
 	
-    Socket connection;
-    PrintWriter output;
-    BufferedReader input;
-    String clientName;
-    static Vector<ServerChat> sockets = new Vector <ServerChat>();
+    static Vector<ServerHandler> sockets = new Vector <ServerHandler>();
     
-    public ServerChat (String clientName, Socket connection, PrintWriter output, BufferedReader input) {
-        this.connection = connection;
-        this.output = output;
-        this.input = input;
-        this.clientName = clientName;
-    }
-    
-    
-    String receive() throws IOException {
-        return input.readLine();
-    }
 
 
-    void send(String msg) {
-        output.println(msg);
-    }
 
-
-    
-    void CloseStreams() throws IOException {
-        output.close();
-        input.close();
-        connection.close();
-        ServerChat.sockets.remove(this);
-        System.out.println("Current Client Count- "+ ServerChat.sockets.size());
-    }
-    
-    
-    
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		try {
-            while (true) {
-                String message = receive();
-
-                if (message.endsWith("Client_wants_to_end_the_connection")) {
-                    message = message.substring(0,message.indexOf('-'))+" has left the server";
-                    for(ServerChat c:ServerChat.sockets) {
-                        if(!c.clientName.equals(clientName)) c.send(message);
-                    }
-                    CloseStreams();
-                    break;
-                }
-
-                for(ServerChat c : ServerChat.sockets) {
-                    if(!c.clientName.equals(clientName))  c.send(message);
-                }
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-		
-	}
 
 }
--- src/ServerHandler.java
+++ src/ServerHandler.java
@@ -1,5 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class ServerHandler implements Runnable {
+    Socket connection;
+    PrintWriter output;
+    BufferedReader input;
+    String clientName;
+    
+    
+    public ServerHandler (String clientName, Socket connection, PrintWriter output, BufferedReader input) {
+        this.connection = connection;
+        this.output = output;
+        this.input = input;
+        this.clientName = clientName;
+    }
+	
+    public String getClientName () {
+    	return clientName;
+    }
+    
+    public String receive() throws IOException {
+        return input.readLine();
+    }
+
+
+   public void send(String msg) {
+        output.println(msg);
+    }
+
+    
+    public void closeStreams() throws IOException {
+        output.close();
+        input.close();
+        connection.close();
+        ServerChat.sockets.remove(this);
+        System.out.println("Current Client Count- "+ ServerChat.sockets.size());
+    }
+    
+ 
+    
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -9,14 +50,14 @@ public class ServerHandler implements Runnable {
 
                 if (message.endsWith("Client_wants_to_end_the_connection")) {
                     message = message.substring(0,message.indexOf('-'))+" has left the server";
-                    for(ServerChat c:ServerChat.sockets) {
-                        if(!c.clientName.equals(clientName)) c.send(message);
+                    for(ServerHandler c : ServerChat.sockets) {
+                        if(!c.getClientName().equals(clientName)) c.send(message);
                     }
                     closeStreams();
                     break;
                 }
 
-                for(ServerChat c : ServerChat.sockets) {
+                for(ServerHandler c : ServerChat.sockets) {
                     if(!c.clientName.equals(clientName))  c.send(message);
                 }
             }
